4dfc3518e44d52b955a03c2bc14e8a576a2aade3
jravshanov <javlon.ravshanov01@gmail.com>
2023-06-24 19:24:29 +0500

    first commit

---
+++ lib/view/countdown_circular.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:parking_countdown/constants/constants.dart';
+import 'dart:math' as math;
+
+
+
+class CountdownCircularPainter extends CustomPainter {
+  final double percentFinished;
+  double radius = 135.0;
+  double strokeWidth = 15.0;
+  CountdownCircularPainter(this.percentFinished);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Offset center = Offset(size.width / 2, size.height / 2);
+    Rect rect = Rect.fromCircle(center: center, radius: radius);
+
+    var rainbowPaint = Paint()
+      ..shader = SweepGradient(colors: kColors).createShader(rect)..style = PaintingStyle.stroke
+      ..strokeWidth = strokeWidth
+      ..strokeCap = StrokeCap.round;
+
+    canvas.drawArc(rect, 0, math.pi * 2, false, Paint()..style = PaintingStyle.stroke..strokeWidth = strokeWidth);
+    canvas.drawArc(rect, 0, percentFinished, false, rainbowPaint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
